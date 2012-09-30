8bf1da7ee6a463bb9810e3d3d1b67b88633b19bf
Eric Crosson <eric.s.crosson@utexas.edu>
2012-09-29 23:18:46 -0500

    In a good state. Hashing with aliases functional, but still working on correct authenticatin syntac for SSH2 module

--- shell.pl
+++ shell.pl
@@ -2,30 +2,43 @@
 use strict;
 
 use Time::HiRes qw(usleep nanosleep);
+use Net::SSH2;
+# If the SSH2 package is not installed, install it with this command
+# curl -L http://cpanmin.us | perl - --sudo Net::SSH2 
 
 # Written by Eric Crosson
-# 29 September 2012
+# Initial commit 29 September 2012
 #
 # This file is the main interface with the user (shell prompt).
 
 # Subroutine prototypes
 sub do_exit();
 sub help_message();
+sub prompt();
+sub sleep();
 sub connect();
 sub disconnect();
-sub sleep();
-sub prompt();
+sub print();
 
+# Constants
+use constant command_not_found => "error: unrecognized command\n";
+
+# The hash map
+# Each of the keys (command from the prompt) corresponds to a 
+# subroutine containing the code to run.
 my %subroutine = (    
     'help'       => \&help_message,
     'connect'    => \&connect,
     'disconnect' => \&disconnect,
+    'print'      => \&print,
     'sleep'      => \&sleep,
     'exit'       => \&do_exit,
     'quit'       => \&do_exit,
 );
 
+my %clients = ();
 
+# Loop to prompt the user for input
 while(1==1) {
     my $action = &prompt();
     chomp($action);
@@ -38,7 +51,7 @@ while(1==1) {
     if (defined $subroutine{$command}) {
 	$subroutine{$command}->(@input);
     } else {
-	print "error: unrecognized command\n";
+	print command_not_found;
     }
 }
 
@@ -55,12 +68,13 @@ sub help_message() {
     }
     
     if(scalar(@_) > 0) {
+# If inquiring about a specific function, pass along the request
 	my ($command) = $_[0];
 	chomp($command);
 	if (defined $subroutine{$command}) {
 	    $subroutine{$command}->("help");
 	} else {
-	    print "error: unrecognized command\n";
+	    print command_not_found;
 	}
     } else {
 print<<END;
@@ -70,6 +84,7 @@ Here is a list of supported commands. Type 'help [command]' to get detailed info
 help\t\tshows this help message
 sleep\t\tsleeps for n milliseconds
 connect\t\tconnects to a phone through ssh
+disconnect\treleases the ssh session connecting this program and a phone
 exit|quit\tleave the shell
 END
 }
@@ -85,6 +100,7 @@ sub do_exit() {
     exit 0;
 }
 
+#TODO-make this work
 sub connect() {
     if($_[0] eq "help") {
 print<<END;
@@ -94,9 +110,20 @@ Disconnect from [host] with 'disconnect [host]'.
 END
 	return;
     }
-	
+    my $ssh2 = Net::SSH2->new();
+    $ssh2->connect($_[0]) or print "Unable to connect to $_[0]" && return;
+
+    print "(optional) Enter an alias for this host: ";
+    my $alias = <>;
+    chomp($alias);
+    if (length($alias) <= 0) { # If alias declined
+	$alias = $_[0];
+    }
+
+    $clients{$alias} = $ssh2;
 }
 
+#TODO-make this work
 sub disconnect() {
     if($_[0] eq "help") {
 print<<END;
@@ -117,4 +144,21 @@ END
 	return;
     }
     usleep($_[0]);
+}
+
+# This subroutine is used only for debugging
+sub print() {
+    if($_[0] eq "help") {
+print<<END;
+Usage: print [host]
+This prints the session associated with a specified host, or all hosts if invoked generically.
+END
+	return;
+    }
+    if (defined $clients{$_[0]}) {
+	print $clients{$_[0]};
+	print "\n";
+    } else {
+	print "The specified host was not found";
+    }
 }
